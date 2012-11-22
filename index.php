b1a1ba2a045b18a649ac06c8885065377096d196
Anders Nawroth <anders@neotechnology.com>
2012-11-22 01:17:07 +0100

    Import av kod.

---
+++ index.php
@@ -0,0 +1,617 @@
+<?php
+
+// AntroPrat Online
+// Copyright Anders Nawroth 2004-2012
+// Licens: AGPL 3.0+, se
+// http://www.gnu.org/licenses/agpl-3.0.html
+
+// *** Ordsamlingar
+// här ligger alla ord som används.
+// det går bra att lägga till nya ord
+// och uttryck direkt, antalet läses av
+// automatiskt. 
+
+$ord1 = array (
+       'Alla antroposofer', 'Alla människor',
+       'Alla klassläsare', 'Alla småbarnsföräldrar',
+       'Alla tonårsföräldrar', 'Alla waldorfföräldrar',
+       'Antroposofen', 'Antroposoferna',
+       'Arkitekten', 'Arkitekterna',
+       'Badterapeuten',
+       'Biodynamikern', 'Bokförläggaren',
+       'Den antroposofiska gruppen',
+       'Den enskilde', 'Den esoteriska lärjungen',
+       'Den esoteriska läraren',
+       'Den invigde', 'De invigda', 'Den klarseende',
+       'Envar',
+       'Eurytmisten', 'Eurytmistudenterna',
+       'Farmaceuten', 'Apotekaren',
+       'Flegmatikern', 'De som är flegmatiska',
+       'Gruppledaren', 'Gruppledarna',
+       'Högskolan', 'Högskolekollegiet',
+       'Individualiteten', 'Individualiteterna',
+       'Klassläsaren', 'Klassläsarna',
+       'Kolerikern', 'De som är koleriska',
+       'Konstnären', 'Konstnärerna',
+       'Kvinnan', 'Kvinnorna',
+       'Körledaren',
+       'Ladugårdskarlen',
+       'Lantbrukaren', 'Lantbrukarna',
+       'Lekskolefröknarna',
+       'Läkaren', 'Läkarna', 'Lärjungen',
+       'Läkeeurytmisten', 'Läkeeurytmisterna',
+       'Läkeeurytmistudenten',
+       'Läkepedagogen', 'Läkepedagogerna',
+       'Massören', 'Matematikläraren',
+       'Medlemmen', 'Medlemmarna',
+       'Melankolikern', 'De som är melankoliska',
+       'Mikaeliten',
+       'Musikern', 'Musikerna', 'Musiklärarna',
+       'Målaren', 'Målningsterapeuten',
+       'Mannen', 'Männen',
+       'Meditatören', 'Meditanten', 'De som mediterar',
+       'Människan', 'Människorna',
+       'Mänskligheten',
+       'Näringsexperten',
+       'Prästen', 'Prästerna',
+       'Sangvinikern', 'De som är sangviniska',
+       'Samfundet',
+       'Sjuksköterskan', 'Sjuksköterskorna',
+       'Skolans kollegium',
+       'Smeden', 'Socialekologen',
+       'Socialterapeuten',
+       'Studiebesökaren',
+       'Styrelsen', 'Styrelsemedlemmen', 'Städaren',
+       'Sällskapet',
+       'Talgestaltaren', 'Talgestaltarna',
+       'Terapeuten', 'Terapeuterna',
+       'Varje människa',
+       'Vårdaren', 'Vårdarna',
+       'Waldorfläraren', 'Waldorflärarna',
+       'Översättaren'
+       );
+
+$ord2 = array (
+       'borde', 'borde förvisso',
+       'borde förstå att',
+       'borde försöka att',
+       'borde kunna', 'borde våga',
+       'borde pröva att',
+       'borde vilja',
+       'borde ha modet att', 'har modet att',
+       'bör',
+       'bör försöka att', 'bör våga', 'bör pröva att',
+       'bör ha modet att',
+       'får', 'får försöka att', 'får förvisso',
+       'förmår', 'förmår förvisso', 'förmår inte', 'försöker',
+       'kan', 'kan förvisso', 'kan försöka att', 'kan våga',
+       'kan ha modet att', 'kan pröva att',
+       'kunde', 'kunde försöka att', 'kunde våga',
+       'kunde ha modet att', 'kunde pröva att',
+       'kommer att', 'kommer förvisso att',
+       'kommer att kunna', 'kommer att våga',
+       'kommer att vilja',
+       'måste', 'måste försöka', 'måste våga', 'måste förvisso',
+       'prövar att',
+       'skall', 'skall försöka att', 'skall våga', 'skall förvisso',
+       'skall ha modet att', 'skall pröva att',
+       'skulle', 'skulle behöva',
+       'skulle försöka',
+       'skulle vilja', 'skulle ha modet att',
+       'skulle kunna', 'skulle våga',
+       'skulle pröva att',
+       'törs', 'törs pröva att', 'törs försöka att',
+       'törs ha modet att',
+       'vill', 'vill förvisso',
+       'vill försöka att', 'vill ha modet att',
+       'vill pröva att',
+       'vågar pröva att', 'vågar försöka att',
+       'vågar ha modet att'
+       );
+// verb
+$ord3 = array (
+       'ana', 'bearbeta', 'befria',
+       'begripa', 'begrunda',
+       'bejaka', 'beliva', 'betrakta', 'beskriva',
+       'bli varse',
+       'bringa i dagen',
+       'erinra sig',
+       'finna', 'fullfölja',
+       'förandliga', 'förespråka',
+       'fördjupa', 'förfina', 'förnya',
+       'förnimma', 'företräda', 'förinnerliga',
+       'förlösa', 'förmedla',
+       'förstå', 'förvandla', 'förverkliga',
+       'genomarbeta', 'genomföra',
+       'genomskåda', 'genomtränga', 'gripa',
+       'gestalta', 'göra sig tankar om',
+       'ha insikt i', 'ha tilltro till',
+       'harmonisera',
+       'imaginera',
+       'impulsera',
+       'individualisera', 'införliva',
+       'initiera', 'inkarnera',
+       'komma till kunskap om', 'komma fram till',
+       'kultivera',
+       'lita på',
+       'metamorfosera',
+       'omvandla',
+       'potentiera',
+       'prägla',
+       'representera', 'respektera',
+       'se', 'skåda',
+       'spiritualisera', 'sträva mot',
+       'studera',
+       'ta upp', 'tänka över',
+       'uppenbara', 'uppleva', 'uppvisa', 'uppnå',
+       'uppöva',
+       'utbilda', 'utveckla', 'utstråla', 'utarbeta',
+       'varsebli',
+       'visa hän till',
+       'visa på',
+       'visa vägen till',
+       'återskapa', 'återfinna',
+       'öppna sig för',
+       'öva', 'öva upp', 'överblicka'
+       );
+// adjektiv
+$ord4 = array (
+       'allmänmänskliga', 'andliga', 'astrala',
+       'aktiva', 'ahrimanska',
+       'belivande', 'bestämda',
+       'esoteriska', 'eteriska', 'exoteriska',
+       'framtida', 'framtidsinriktade', 'förandligade',
+       'förbindande', 'friskgörande',
+       'fördomsfria', 'fria', 'förlösande',
+       'fröartade',
+       'gemenskapsbildande', 'goetheanistiska',
+       'gudomliga', 'goda',
+       'halvmedvetna',
+       'harmoniska', 'helande', 'hierarkiska',
+       'högre',
+       'imaginativa', 'inre', 'innerliga',
+       'inspirativa', 'intuitiva', 'innevarande',
+       'individuella', 'inkarnerade',
+       'jagmedvetna',
+       'jordiska',
+       'karmiska', 'kristliga',
+       'konstnärliga', 'kosmiska',
+       'kvalitativa',
+       'känslomässiga',
+       'lemniskatiska', 'livgivande', 'ljusfyllda',
+       'levande', 'luciferiska', 'luftartade',
+       'meditativa', 'medvetna', 'moderna', 'modiga', 'mikaeliska',
+       'moraliska',
+       'normalt organiserade',
+       'nutida',
+       'ockulta', 'omedvetna',
+       'organiska',
+       'planetariska',
+       'positiva',
+       'praktiska',
+       'reinkarnerade',
+       'rytmiska', 'rörliga',
+       'sanna', 'sociala',
+       'socialekologiska', 'socialterapeutiska',
+       'sovande',
+       'spirituella',
+       'tankemässiga', 'tankeväckande',
+       'terapeutiska', 'tregrenade',
+       'tidsenliga',
+       'uppbyggande',
+       'vakna', 'verkliga', 'viljeartade',
+       'visionära',
+       'väsensartade', 'välsignelsebringande',
+       'vätskeartade', 'värmeartade',
+       'yttre',
+       'ödesmässiga'
+       );
+
+$denord = array (
+       'andevärlden', 'andeläran', 'andligheten', 'antroposofin',
+       'andemänniskan', 'aktiviteten',
+       'astralkroppen', 'astraliteten',
+       'betraktelsen',
+       'bilden',
+       'bärkraften',
+       'civilisationsprincipen',
+       'eldanden',
+       'elementaranden',
+       'esoteriken', 'eterkroppen', 'eterkraften',
+       'evolutionen',
+       'filosofin', 'framtidsimpulsen', 'folkanden', 'formkraften',
+       'frihetsutvecklingen', 'frihetsprocessen', 'frihetsevolutionen',
+       'frågeställningen', 'föreställningen',
+       'fysiska kroppen',
+       'fördjupningen',
+       'förståndssjälen',
+       'förståelsen',
+       'gemenskapen', 'gestalten', 'gnomen', 'gruppen',
+       'grundstensmeditationen', 'grundstenen',
+       'harmonin', 'helheten',
+       'höjdpunkten',
+       'iakttagelsen',
+       'idealismen',
+       'imaginationen', 'impulsen',
+       'initiationsprincipen', 'inkarnationen',
+       'inspirationen', 'intuitionen',
+       'invigningen', 'initiativkraften',
+       'jagimpulsen', 'jagutvecklingen',
+       'jagkärnan',
+       'karman', 'kulten', 'kulturimpulsen',
+       'klasstimmen',
+       'kunskapen', 'kunskapsvägen',
+       'Kristusimpulsen', 'Kristusanden',
+       'Kristusprincipen',
+       'kvaliteten', 'kreativiteten',
+       'kroppsligheten',
+       'känslosjälen',
+       'lagbundenheten',
+       'lemniskatan',
+       'livsanden', 'livsprocessen',
+       'luftanden',
+       'meditationen', 'metamorfosen',
+       'medvetenhetstidsåldern', 'medvetenhetssjälen',
+       'medvetenhetsprocessen',
+       'Mikaelimpulsen', 'Mikaeltidsåldern', 'Mikaelstiden',
+       'modkraften',
+       'mänskligheten', 'mänsklighetsutvecklingen', 'människokunskapen',
+       'nåden', 'näringen', 'näringsprocessen',
+       'ockultismen', 'omkretsen', 'organisationskulturen',
+       'omdömesbildningen',
+       'periferin', 'porten', 'principen',
+       'reinkarnationen',
+       'realiteten',
+       'riktningskraften',
+       'rytmen',
+       'sanningen', 'salamandern',
+       'självinsikten', 'självkännedomen',
+       'själsläran', 'själsutvecklingen',
+       'själsrörelsen', 'själsvärlden',
+       'själsaktiviteten', 'själsverksamheten',
+       'skolningsvägen',
+       'själskroppen', 'själsformationen',
+       'sylfen',
+       'sällskapsbildningen',
+       'tankeförmågan', 'tankeförbindelsen',
+       'tidsanden', 'tidsåldern',
+       'undinen',
+       'uppenbarelsen',
+       'ursprungskällan', 'källan',
+       'vattenanden',
+       'verksamheten',
+       'vetskapen',
+       'verkligheten', 'visheten', 'viljenaturen',
+       'visionen', 'vägen',
+       'världsimpulsen', 'världsimaginationen',
+       'världsprocessen', 'världsutvecklingen',
+       'världsskaparkraften',
+       'värmekvalitén',
+       'återblicken', 'åskådningen',
+       'ängeln',
+       'ödeskonstellationen', 'ödesförbindelsen',
+       'överblicken', 'övningsvägen'
+       );
+
+$detord = array (
+       'andeskådandet',
+       'andesjälvet',
+       'ande-erinrandet', 'ande-besinnandet',
+       'elementet',
+       'flödet',
+       'fältet', 'förloppet',
+       'framåtskridandet',
+       'frihetselementet',
+       'förkroppsligandet',
+       'förnimmandet',
+       'förtroendet',
+       'handlandet',
+       'helandet', 'hjärtat',
+       'helhetsperspektivet',
+       'iakttagelseresultatet',
+       'idealet', 'initiativet',
+       'jaget', 'jagmedvetandet',
+       'jordelivet',
+       'klarseendet', 'kraftfältet',
+       'Kristusundret',
+       'kännandet',
+       'luftelementet',
+       'läkemedlet',
+       'medlidandet', 'medvetandet',
+       'mänsklighetsidealet',
+       'området',
+       'perspektivet',
+       'skapandet',
+       'skeendet', 'skådandet', 'sökandet',
+       'stadiet',
+       'själsinnehållet', 'själsområdet',
+       'tankeinnehållet',
+       'tidsödet', 'tidsrummet',
+       'tålamodet', 'tänkandet',
+       'undret',
+       'ursprunget', 'uppdraget',
+       'varat', 'varseblivandet', 'vattenelementet',
+       'verkandet', 
+       'viljandet',
+       'vishetsljuset',
+       'värmeelementet',
+       'årsloppet', 'årskretsloppet',
+       'återförkroppsligandet',
+       'ödet', 'övandet'
+       );
+
+// initiera mallar
+
+$allamallar = array (
+'vanlig', 'mallar',
+'tråkig', 'trakigt',
+'vit-svart', 'vittsvart',
+'utskriftsvänlig', 'utskrift',
+'akvarellig', 'akva',
+'serieartad', 'serie',
+'skrivmaskinig', 'skrivmaskin'
+);
+
+$valen = array ();
+while (sizeof($allamallar))
+{
+    $ettval = array ();
+    $ettval['namn'] = array_shift($allamallar);
+    $ettval['mall'] = array_shift($allamallar);
+    $valen[] = $ettval;
+}
+
+$val = 3;
+if (isset($_POST['inkarnation']))
+{
+    $val = (integer) $_POST['inkarnation'];
+    if (!isset($valen[$val]))
+    {
+        $val = 3;
+    }
+}
+
+if (isset($_GET['lista']))
+{
+    $val = 3;
+}
+
+
+$mall = $valen[$val]['mall'] . '.css';
+// special för utskrifter:
+if (array_search($mall, array ('', 'utskrift.css', 'skrivmaskin.css')))
+{
+    $utskrift = TRUE;
+} else
+{
+    $utskrift = FALSE;
+}
+
+// bygg select med nuvarande sida som förval
+// för nästa inkarnation
+$select = '<select name="inkarnation">' . "\n\n";
+foreach ($valen as $nummer => $ettval)
+{
+    if ($nummer == $val)
+    {
+        $vald = ' selected="selected"';
+    } else
+    {
+        $vald = '';
+    }
+    $select .= '<option value="' . $nummer . '"' . $vald . '>' . "\n"
+    . $ettval['namn'] . "\n"
+    . '</option>' . "\n\n";
+}
+$select .= '</select>' . "\n\n";
+
+
+// *** Sidstart
+// skickar headers + inleder sidan.
+
+function HttpDate($offset = 0)
+{
+    return gmdate('D, d M Y H:i:s', time() + $offset) . ' GMT';
+}
+
+header('Window-Target: _top');
+header('Content-Type: text/html; charset=iso-8859-1');
+header('Content-Script-Type: text/javascript');
+header('Content-Style-Type: text/css');
+header('Content-Language: sv');
+$tmp = HttpDate();
+header('Date: ' . $tmp);
+header('Last-Modified: ' . $tmp);
+header('Expires: ' . HttpDate(-3600));
+header('Cache-Control: no-cache, must-revalidate');
+header('Pragma: no-cache');
+echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+       "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+
+<head>
+   <link href="', $mall, '" rel="stylesheet" type="text/css">
+   <meta http-equiv="imagetoolbar" content="no">
+   <meta name="Author" content="Anders Nawroth">
+   <meta name="KeyWords" content="antroposofi,antroposof,antroposofer,antroposofisk,fras">
+   <meta name="Description" content="AntroPrat Online, en antroposofiskt orienterad frasmaskin.">
+   <title>AntroPrat Online</title>
+</head>
+
+<body>
+
+<div>
+
+<h1>AntroPrat Online</h1>
+
+<form method="post" action="/antroprat/">
+
+Välj en
+
+', $select,
+'inkarnation, och
+
+<input type="submit" value="reinkarnera">
+
+AntroPrat!
+
+</form>
+
+<ul>
+
+<li>
+Genom <em>databehandling</em> av innehållet i en större mängd antroposofiska
+föredrag, gruppaftnar, artiklar och medarbetarmöten har AntroPrat kunnat förverkligas.
+</li>
+
+<li>
+Genom en <em>sinnrik slumpmässig mekanism</em> skapar AntroPrat fraser med
+antroposofisk inriktning, färdiga att klistra in i din ordbehandlare.
+</li>
+
+<li>
+AntroPrat möjliggör ett <em>vidgat medvetande</em>.
+</li>
+
+</ul>
+
+';
+
+if (isset($_GET['lista']))
+{
+    lista($ord1);
+    lista($ord2);
+    lista($ord3);
+    lista($ord4);
+    lista($denord);
+    lista($detord);
+    exit;
+}
+
+function lista($ordlista)
+{
+    echo '<p>';
+    echo implode(', ', $ordlista);
+    echo '</p>';
+}
+
+// *** Frasmotor
+// skapar fraser
+
+// inställningar och initieringar:
+
+$antalOrd1 = count($ord1);
+$antalOrd2 = count($ord2);
+$antalOrd3 = count($ord3);
+$antalOrd4 = count($ord4);
+$antalDenord = count($denord);
+$antalDetord = count($detord);
+$antalDenDetord = $antalDenord + $antalDetord;
+
+$totalaFraser = $antalOrd1 * $antalOrd2 * $antalOrd3 * $antalOrd4 *
+  $antalDenDetord;
+$totalaFraser = number_format($totalaFraser, 0, ', ', ' ');
+
+$gamlaFraser = array();
+$Fraser = array();
+
+$kollaAntal = 8; // hur många bakåt som kollas för likhet
+$maxFrasLikhet = 24; // max likhet mellan strängarna = 10-25 försök/fras
+$maxInternLikhet = 6; // max liket mellan orden i en fras
+$pratAntal = 19; // antal fraser som produceras
+
+// vi skapar begärt antal fraser
+while (count($Fraser) < $pratAntal)
+{
+    $nyttOrd1 = $ord1[array_rand($ord1)];
+    $nyttOrd2 = $ord2[array_rand($ord2)];
+    $nyttOrd3 = $ord3[array_rand($ord3)];
+    $valDenDet = rand(1, $antalDenDetord) > $antalDenord;
+    do
+    {
+        $nyttOrd4 = $ord4[array_rand($ord4)];
+        if ($valDenDet)
+        {
+            $nyttDenDet = $detord[array_rand($detord)];
+        } else
+        {
+            $nyttDenDet = $denord[array_rand($denord)];
+        }
+        $internLikhet = similar_text($nyttOrd3, $nyttOrd4)
+        + similar_text($nyttOrd3, $nyttDenDet)
+        + similar_text($nyttOrd4, $nyttDenDet);
+    } while ($internLikhet > $maxInternLikhet);
+    $enFras = $nyttOrd1 . ' ' . $nyttOrd2 . ' ' . $nyttOrd3;
+    if ($valDenDet)
+    {
+        $enFras .= ' det ';
+    } else
+    {
+        $enFras .= ' den ';
+    }
+    $enFras .= $nyttOrd4 . ' ' . $nyttDenDet . '.';
+
+    // dags att kolla om meningen är för lik de gamla meningarna.
+    $lika = FALSE;
+    $testFraser = $gamlaFraser;
+    while ((count($testFraser) > 0) and (! $lika))
+    {
+        if (similar_text($enFras, array_pop($testFraser)) > $maxFrasLikhet)
+        {
+            $lika = TRUE;
+        }
+    }
+
+    if (! $lika)
+    {
+        $Fraser[] = $enFras;
+        if (array_unshift($gamlaFraser, $enFras) > $kollaAntal)
+        {
+            // arrayen ska inte växa mer nu
+            array_pop($gamlaFraser);
+        }
+    }
+}
+
+// dags att leverera fraserna.
+$Fraser = ' &ndash; ' . implode("\n &ndash; ", $Fraser);
+
+echo '<p>
+
+Det totala antalet möjliga unika fraser i <em>AntroPrat Online</em>
+<br />är nu '
+ . '<em>' . $totalaFraser . '</em>'
+. ' stycken!
+<br />Fraserna nedan förnyas varje gång sidan reinkarnerar!
+
+</p>
+
+';
+
+if ($utskrift)
+{
+    echo nl2br($Fraser . "\n\n");
+} else
+{
+    echo '<textarea rows="7" cols="72" readonly="readonly">' . "\n" . $Fraser . '
+</textarea>';
+}
+
+echo '
+<div>
+
+<address>&copy; nawroth.com 2004-2012</address>
+
+<a href="https://github.com/angeloi/antroprat"><img src="github.jpg" alt="Koden bakom sidan finns på GitHub" height="48" width="48" /></a>
+
+</div>
+
+</div>
+
+</body>
+
+</html>
+';
+
+?>
